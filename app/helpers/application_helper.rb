1a61a123808227faa3eb27296a71d6d67417e81e
超高 <chaogao@MacBook-Pro-2.local>
2020-08-11 13:34:21 +0900

    homepageレイアウト

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')  # full_titleメソッドを定義
+    base_title = '楽ダイエット'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}" 
+    end
+  end
 end
