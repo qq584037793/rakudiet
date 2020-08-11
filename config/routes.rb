c4b46ffeedac8a98d165d98a2bde1f7ea4a4d1d3
超高 <chaogao@MacBook-Pro-2.local>
2020-08-11 21:04:35 +0900

    userコントローラ、モデルand日本語化対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   root 'static_pages#home'
-  get :about,        to: 'static_pages#about'
+  get :about,  to: 'static_pages#about'
+  get :signup, to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
