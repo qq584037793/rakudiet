efbf3fbe027ce8e6dba8949929b17536f21f2ba0
超高 <chaogao@MacBook-Pro-2.local>
2020-09-13 19:41:00 +0900

    修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
     belongs_to :user
     belongs_to :cook
+    default_scope -> { order(created_at: :desc) }  
     validates :user_id, presence: true
     validates :cook_id, presence: true  
 end
